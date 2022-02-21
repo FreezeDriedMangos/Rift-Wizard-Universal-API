8897d43ba2fbc9a12641894e3f1022a9f9615992
anotak <anotak@users.noreply.github.com>
2022-02-21 09:53:36 -0600

    the LevelGen.roll_shrine / LevelGen.random_spell_tag still pointed at the original one from Shrines

--- ShrinesOverrides.py
+++ ShrinesOverrides.py
@@ -1,7 +1,10 @@
 
 import Shrines as Shrines
+import LevelGen as LevelGen
 import mods.API_Universal.APIs.API_LevelGenProps.API_LevelGenProps as API_LevelGenProps
 import mods.API_Universal.APIs.API_Spells.API_Spells as API_Spells
 
 Shrines.roll_shrine = API_LevelGenProps.roll_shrine
 Shrines.random_spell_tag = API_Spells.random_spell_tag
+LevelGen.roll_shrine = API_LevelGenProps.roll_shrine
+LevelGen.random_spell_tag = API_Spells.random_spell_tag
